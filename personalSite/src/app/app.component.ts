11c1de427399bf8758b17a09b9164a3753b797e1
Joanne Nguyen <jnnguyen@iastate.edu>
2023-08-22 21:12:20 -0500

    refactored to loops

--- personalSite/src/app/app.component.ts
+++ personalSite/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personalSite';
+
+  langs:string[] = [
+    'C', 'C++', 'Java', 'JavaScript', 'TypeScript', 'SQL', 'C#', "HTML5", "Razor", 'SCSS', 'Prolog'
+  ];
+
+  frameworks:string[] = [
+    'Volley', 'MySQL', 'Bootstrap', 'Angular', 'Pandas', 'Seaborn', 'Tensorflow'
+  ];
+
+  tools:string[] = [
+    'Figma', 'Umbraco CMS', 'Postman', 'Intuiface', 'Adobe Photoshop', 'Adobe Illustrator', 'Agile', 'Git', 'Azure DevsOps'
+  ];
+
+  desc:{[key:string]: string} = {
+    C: "Both C and C++ I have worked with during my COMS327 class where we have to implement a Pokémon game, using NCurses and applicable data structures.",
+    "C++": "Both C and C++ I have worked with during my COMS327 class where we have to implement a Pokémon game, using NCurses and applicable data structures.",
+    "Java": "I have worked with Java for most of my projects in" +  ("COMS309 - Sweet Valley").bold()
+  }
 }
