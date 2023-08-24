503a6d2c793be4e899f1826dba4f5bebf14dedfb
Joanne Nguyen <jnnguyen@iastate.edu>
2023-08-24 13:42:20 -0500

    more content

--- personalSite/src/app/app.component.ts
+++ personalSite/src/app/app.component.ts
@@ -21,9 +21,12 @@ export class AppComponent {
   ];
 
   desc:{[key:string]: string} = {
-    C: "Both C and C++ I have worked with during my COMS327 class where we have to implement a Pokémon game, using NCurses and applicable data structures.",
-    "C++": "Both C and C++ I have worked with during my COMS327 class where we have to implement a Pokémon game, using NCurses and applicable data structures.",
-    "Java": "I have worked with Java for most of my projects in" +  ("COMS309 - Sweet Valley").bold()
+    "C": "I got to learn C in my COMS327 class where we have to implement a Pokémon game, using NCurses and applicable data structures. I used C for the first part of the application, where most of the logic was developed.",
+    "C++": "I got to learn C++ in my COMS327 class where we have to implement a Pokémon game, using NCurses and applicable data structures. I used C++ for the second half of the application, where a lot more front end components were tuned on.",
+    "Java": "I have worked with Java for most of my projects in my Android Development class COMS309 - a spin off from Candy Land mobile game - and in COMS472 in an application on helping an AI play Tic Tac Toe basing on AIMA's github code. Aside from that, I used Java the most for Test Driven Development.",
+    "JavaScript": "I have learned JavaScript in my COMS319 Web Development class. In this class, we developed a top-down hand-drawn bullet hell game, including a duck and onions for enemies.",
+    "TypeScript": "I had the opportunity to learn Angular and TypeScript during a programming challenge for my current internship at RDI. After that, I used this knowledge to try to develop a weather app during Hackathon Spring 2023",
+    "SQL": "I have experience with SQL on creating databases from datasheets in my Database Management class. In which, I used "
   }
 
   onSelect(lang:string):void {
