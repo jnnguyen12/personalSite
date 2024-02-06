b75326c2c872e9793b2f35ff94804cc88fa02b80
Joanne Nguyen <jnnguyen@iastate.edu>
2024-02-06 01:49:05 -0600

    image link fix

--- src/app/art-list/art-list.component.ts
+++ src/app/art-list/art-list.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class ArtListComponent {
     
   imgLinks: {name: string, medium: string, year: number, link: string}[] = [
-    {name: "Fish girl", medium: 'Watercolor on canvas', year: 2019, link: '../assets/img/fishgirl.JPG'},
-    {name: "Eye", medium: 'Oil on canvas', year: 2020, link: '../assets/img/eye.jpeg'},
-    {name: "Girls on bicycle", medium: 'Watercolor', year: 2018, link: '../assets/img/girls.JPG'},
-    {name: "Ocean", medium: 'Oil on canvas', year: 2019, link: '../assets/img/ocean.jpg'},
-    {name: "Orchid", medium: 'Micron Ink', year: 2020, link: '../assets/img/orchid.png'},
-    {name: "Pottery hands", medium: 'Watercolor', year: 2018, link: '../assets/img/hands.jpg'},
+    {name: "Fish girl", medium: 'Watercolor on canvas', year: 2019, link: 'assets/img/fishgirl.JPG'},
+    {name: "Eye", medium: 'Oil on canvas', year: 2020, link: 'assets/img/eye.jpeg'},
+    {name: "Girls on bicycle", medium: 'Watercolor', year: 2018, link: 'assets/img/girls.JPG'},
+    {name: "Ocean", medium: 'Oil on canvas', year: 2019, link: 'assets/img/ocean.jpg'},
+    {name: "Orchid", medium: 'Micron Ink', year: 2020, link: 'assets/img/orchid.png'},
+    {name: "Pottery hands", medium: 'Watercolor', year: 2018, link: 'assets/img/hands.jpg'},
   ]
 }
