a2c6d9ad6b9183d0ec37bd05380de1222d9c44c9
jnnguyen12 <105896253+jnnguyen12@users.noreply.github.com>
2024-09-06 23:49:01 -0500

    done

--- src/app/scroll-animation.directive.ts
+++ src/app/scroll-animation.directive.ts
@@ -5,6 +5,8 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/
 })
 export class ScrollAnimationDirective {
   @Input() animationClass: string = 'fade-up';
+  @Input() animationDelay: string = '0s';
+  @Input() animationStartHeight: string = '0.65';
   private isVisible = false;
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
@@ -15,18 +17,21 @@ export class ScrollAnimationDirective {
   }
 
   ngOnInit() {
+    this.setDelay();
     this.checkVisibility();
+  }
+
+  private setDelay() {
+    console.warn(this.animationDelay)
+    this.renderer.setStyle(this.el.nativeElement, 'transition-delay', this.animationDelay);
   }
 
   private checkVisibility() {
     const rect = this.el.nativeElement.getBoundingClientRect();
-    const topShown = rect.top > 0 && rect.top < window.innerHeight * 0.65;
-
-    console.log(topShown);
+    const topShown = rect.top > 0 && rect.top < window.innerHeight * parseFloat(this.animationStartHeight);
 
     if (topShown && !this.isVisible)
     {
-      console.log("here");
       this.isVisible = true;
       this.renderer.addClass(this.el.nativeElement, this.animationClass);
     }
