68dd9f14e1b7ed10e3b0689cc495789eb4552a74
jnnguyen12 <105896253+jnnguyen12@users.noreply.github.com>
2024-12-17 19:14:10 -0600

    changed scroll delay

--- src/app/scroll-animation.directive.ts
+++ src/app/scroll-animation.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/
 export class ScrollAnimationDirective {
   @Input() animationClass: string = 'fade-up';
   @Input() animationDelay: string = '0s';
-  @Input() animationStartHeight: string = '0.7';
+  @Input() animationStartHeight: string = '0.8';
   private isVisible = false;
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
